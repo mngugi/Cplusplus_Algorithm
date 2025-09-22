b3fd37bd42d623e3f6af1eaa06e1681078b3ff84
mngugi <mngugi7@hotmail.com>
2025-09-22 20:45:28 +0300

    Update adjacent.cpp

--- adjacent.cpp
+++ adjacent.cpp
@@ -5,6 +5,10 @@ adjacent_find
 */
 //reference:
 // https://cplusplus.com/reference/algorithm/adjacent_find/
+/*
+- start: the first iterator to the first pair of elements that are equal to each other
+- end: the last iterator to the last pair of elements that are equal to each other.
+*/
 
 #include <iostream>
 #include <vector>
@@ -13,12 +17,21 @@ using namespace std;
 
 int main() {
   vector<string> cars = {"Volvo", "BMW", "Ford", "Ford", "Mazda"};
+  // find the first pair of elements that are equal to each other
+  // in this case, the first pair of elements that are equal to each other is "Ford"
+  // so the iterator will be the second "Ford"
+  // and the value of the iterator will be "Ford"
+  // the second pair of elements that are equal to each other is "Mazda"
+  // so the iterator will be the third "Mazda"
+  // and the value of the iterator will be "Mazda"
+   
+  
   auto it = adjacent_find(cars.begin(), cars.end());
 
   if (it != cars.end()) {
-    cout << *it << " appears twice in a row\n";
+    cout << *it << " Appears twice in a row\n";
   } else {
-    cout << "There are no adjacent elements\n";
+    cout << "There are no Adjacent elements\n";
   }
   return 0;
 }
