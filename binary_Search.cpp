168a8fb9a897708b361f52da2efee6feb494d309
mngugi <mngugi7@hotmail.com>
2025-10-07 23:37:39 +0300

    binary_Search

--- binary_Search.cpp
+++ binary_Search.cpp
@@ -2,3 +2,23 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<int > numbers = {2,9,3,4,5,7};
+    sort(numbers.begin(), numbers.end());
+
+    if (binary_search( numbers.begin(), numbers.end(), 4)) {
+            cout << "the number 4 was found";
+
+    } else {
+
+            cout << "the number 4 was not found.";
+
+    }
+
+    return 0;
+
+
+}
